986252629edfec9cc1b575b59a23331d7ef79593
harry688tan96 <harry688tan@gmail.com>
2017-05-17 13:25:14 -0400

    Completed assignment_1_q3

--- assignment1/q3_word2vec.py
+++ assignment1/q3_word2vec.py
@@ -15,19 +15,21 @@ def normalizeRows(x):
     """
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    x_dimension = x.shape[0]
+    x_norm = np.sqrt(np.sum(x**2, axis=1)).reshape((x_dimension,1))
+    x = x / x_norm
     ### END YOUR CODE
 
     return x
 
 
 def test_normalize_rows():
-    print "Testing normalizeRows..."
+    print "--- Testing normalizeRows... ---"
     x = normalizeRows(np.array([[3.0,4.0],[1, 2]]))
     print x
     ans = np.array([[0.6,0.8],[0.4472136,0.89442719]])
     assert np.allclose(x, ans, rtol=1e-05, atol=1e-06)
-    print ""
+    print "--- End of normalizeRows test ---\n"
 
 
 def softmaxCostAndGradient(predicted, target, outputVectors, dataset):
@@ -58,7 +60,15 @@ def softmaxCostAndGradient(predicted, target, outputVectors, dataset):
     """
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    probs = softmax(np.dot(predicted, outputVectors.T))
+    cost = -np.log(probs[target])
+
+    scores_diff = probs
+    scores_diff[target] -= 1
+    gradPred = np.dot(scores_diff, outputVectors) # derived in q3a. Note: gradPred should have the same dim. as the predicted vector since we are differentiating w.r.t vc.
+    
+    dim_m, dim_n = outputVectors.shape
+    grad = np.dot(scores_diff.reshape((dim_m, 1)), predicted.reshape((1, dim_n))) # derived in q3b. Note: grad should have the same dim. as the outputVectors since we are differentiating w.r.t uw.
     ### END YOUR CODE
 
     return cost, gradPred, grad
@@ -92,17 +102,38 @@ def negSamplingCostAndGradient(predicted, target, outputVectors, dataset,
 
     # Sampling of indices is done for you. Do not modify this if you
     # wish to match the autograder and receive points!
-    indices = [target]
+    indices = []
     indices.extend(getNegativeSamples(target, dataset, K))
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    # COMPUTING SOFTMAX COST
+    target_row_vector = outputVectors[target]
+    sigmoid_uoT_vc = sigmoid(np.dot(target_row_vector, predicted)) # returns one value
+    
+    neg_sample_vectors = outputVectors[indices]
+    sigmoid_ukT_vc = sigmoid(-np.dot(neg_sample_vectors, predicted))
+    
+    cost = -np.log(sigmoid_uoT_vc) - np.sum(np.log(sigmoid_ukT_vc))
+    # END OF COMPUTING SOFTMAX COST
+
+    # COMPUTING GRADIENT FOR INPUT AND OUTPUT VECTORS
+    sigmoid_minus_one = sigmoid_uoT_vc - 1
+    sigmoid_neg_minus_one = sigmoid_ukT_vc - 1
+    
+    gradPred_first_term = sigmoid_minus_one * target_row_vector
+    gradPred_second_term = np.dot(sigmoid_neg_minus_one, neg_sample_vectors)
+    gradPred = gradPred_first_term - gradPred_second_term # derived in q3c. Note: gradPred should have the same dim. as the predicted vector since we are differentiating w.r.t vc.
+
+    grad = np.zeros(outputVectors.shape)
+    grad[target] = np.dot(sigmoid_minus_one,predicted)
+    for index, pos in enumerate(indices):
+    	grad[pos] += -(sigmoid_neg_minus_one)[index] * predicted
+    # COMPUTING GRADIENT FOR INPUT AND OUTPUT VECTORS
     ### END YOUR CODE
 
     return cost, gradPred, grad
 
-
-def skipgram(currentWord, C, contextWords, tokens, inputVectors, outputVectors,
+def skipgram(centerWord, C, contextWords, tokens, inputVectors, outputVectors,
              dataset, word2vecCostAndGradient=softmaxCostAndGradient):
     """ Skip-gram model in word2vec
 
@@ -130,14 +161,33 @@ def skipgram(currentWord, C, contextWords, tokens, inputVectors, outputVectors,
     gradIn = np.zeros(inputVectors.shape)
     gradOut = np.zeros(outputVectors.shape)
 
+    ### DEBUGGING PURPOSE
+    # print "Center word: ", centerWord
+    # print "Context size: ", C
+    # print "Context words: ", contextWords
+    # print "Tokens with index: ", tokens
+    # print "InputVectors: ", inputVectors
+    # print "CenterWord row vector: ", inputVectors[tokens[centerWord]]
+    # print "Output vectors: ", outputVectors
+    # print "Dataset: ", dataset
+    ### END OF DEBUGGING
+
     ### YOUR CODE HERE
-    raise NotImplementedError
+    centerWord_pos = tokens[centerWord]
+    centerWord_vector = inputVectors[centerWord_pos]
+    
+    for context_word in contextWords:
+    	target = tokens[context_word]
+    	softmax_cost, grad_pred, grad_out = word2vecCostAndGradient(centerWord_vector, target, outputVectors, dataset)
+    	cost += softmax_cost
+    	gradIn[centerWord_pos] += grad_pred
+    	gradOut += grad_out
     ### END YOUR CODE
 
     return cost, gradIn, gradOut
 
 
-def cbow(currentWord, C, contextWords, tokens, inputVectors, outputVectors,
+def cbow(centerWord, C, contextWords, tokens, inputVectors, outputVectors,
          dataset, word2vecCostAndGradient=softmaxCostAndGradient):
     """CBOW model in word2vec
 
@@ -155,8 +205,20 @@ def cbow(currentWord, C, contextWords, tokens, inputVectors, outputVectors,
     gradOut = np.zeros(outputVectors.shape)
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    indices = [tokens[w] for w in contextWords]  
+    context_words_vector = inputVectors[indices]
+    context_words_vector = np.sum(context_words_vector, axis=0)
+    target = tokens[centerWord]    
+
+    softmax_cost, grad_in, grad_out = word2vecCostAndGradient(context_words_vector, target, outputVectors, dataset)
+    # The following for loop seems unavoidable. We couldn't perform this operation: 
+    #    ```gradIn[indices] += grad_in``` cuz numpy doesn't seem to update (add) rows of elements in place.
+    for index in indices:
+        gradIn[index] += grad_in
+
     ### END YOUR CODE
+    cost = softmax_cost
+    gradOut = grad_out
 
     return cost, gradIn, gradOut
 
@@ -194,7 +256,7 @@ def word2vec_sgd_wrapper(word2vecModel, tokens, wordVectors, dataset, C,
 
 def test_word2vec():
     """ Interface to the dataset for negative sampling """
-    dataset = type('dummy', (), {})()
+    dataset = type('dummy', (), {})()  # return a new type object
     def dummySampleTokenIdx():
         return random.randint(0, 4)
 
